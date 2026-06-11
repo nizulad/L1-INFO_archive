68c135250c0a98b163595818378e9c4c84b599f3
nizulad <pccenrico@gmail.com>
2026-06-11 18:00:07 +0100

    Update SCRIPT_A_URL to new endpoint

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Script A is our single source of truth.
-const SCRIPT_A_URL = "https://script.google.com/macros/s/AKfycbyNmGxiTpfpVxlbauyxZhGfHBxyEuLJBUZdAYAMbQUf3pu2gZtvPfCoLPSnjK5JYufZjA/exec";
+const SCRIPT_A_URL = "https://script.google.com/macros/s/AKfycbzULhdk6PKSHM6JDhEoK20OG2yXiCLYXC6gQJ7xmuJKuSPTB4SDuRcMvrPA4Wu47BPm1Q/exec";
 
 let GLOBAL_FILE_CACHE = {};
 let isSyncing = false;
